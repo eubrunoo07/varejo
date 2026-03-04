5f8a6bf9a2b2a70e6e6c93f76f3e0c52c1028329
eubrunoo07 <devbruninn@outlook.com>
2026-03-04 19:08:47 -0300

    feature: get all low stock products endpoint

--- inventory-service/src/main/java/eubrunoo07/projects/inventory_service/controller/StockController.java
+++ inventory-service/src/main/java/eubrunoo07/projects/inventory_service/controller/StockController.java
@@ -1,14 +1,15 @@
 package eubrunoo07.projects.inventory_service.controller;
 
+import eubrunoo07.projects.inventory_service.dto.LowStockProductDTO;
 import eubrunoo07.projects.inventory_service.dto.StockRequestDTO;
 import eubrunoo07.projects.inventory_service.dto.StockResponseDTO;
 import eubrunoo07.projects.inventory_service.mapper.StockMapper;
 import eubrunoo07.projects.inventory_service.service.StockService;
 import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -32,6 +33,11 @@ public class StockController {
     @GetMapping("/{productId}")
     public ResponseEntity<StockResponseDTO> stockByProductId(@PathVariable UUID productId){
         return ResponseEntity.ok(stockMapper.map(stockService.stockByProductId(productId)));
+    }
+
+    @GetMapping("/low-inventory")
+    public ResponseEntity<List<LowStockProductDTO>> getLowInventoryProducts(){
+        return ResponseEntity.ok(stockService.getLowInventoryProducts());
     }
 
     @PatchMapping("/{productId}/safety-stock")
--- inventory-service/src/main/java/eubrunoo07/projects/inventory_service/repository/StockRepository.java
+++ inventory-service/src/main/java/eubrunoo07/projects/inventory_service/repository/StockRepository.java
@@ -3,11 +3,19 @@ package eubrunoo07.projects.inventory_service.repository;
 import eubrunoo07.projects.inventory_service.model.Stock;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface StockRepository extends JpaRepository<Stock, UUID> {
     Optional<Stock> findByProductId(UUID productId);
     boolean existsByProductId(UUID productId);
+    @Query("""
+       SELECT s.productId
+       FROM Stock s
+       WHERE (s.quantityGondola + s.quantityWarehouse) < s.minimumSafetyStock
+       """)
+    List<UUID> findProductsInLowStock();
 }
